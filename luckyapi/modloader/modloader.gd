9ce0465434db62e670d38827d9703f307856552e
Valgo <valgohopscotch@gmail.com>
2021-05-18 15:15:09 -0600

    Modloader now creates mod folder if it doesn't exist

--- luckyapi/modloader/modloader.gd
+++ luckyapi/modloader/modloader.gd
@@ -161,6 +161,7 @@ func patch_symbol(symbol_patch, id):
 func before_start():
     print("LuckyAPI MODLOADER > Initializing LuckyAPI " + modloader_version + "...")
     ensure_dir_exists("user://_luckyapi_patched")
+    ensure_dir_exists("user://mods")
 
     var main_script := extract_script(load("res://Main.tscn"), "Main").source_code
     var regex := RegEx.new()
