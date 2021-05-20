8e6513e0a41c8a1ce6a5086f785ecfa371336fbf
Valgo <valgohopscotch@gmail.com>
2021-05-20 11:52:01 -0600

    Add 0.6.5 to the list of allowed internal versions

--- luckyapi/modloader/modloader.gd
+++ luckyapi/modloader/modloader.gd
@@ -5,7 +5,7 @@ const ModSymbol = preload("res://modloader/ModSymbol.gd")
 const SymbolPatcher = preload("res://modloader/SymbolPatcher.gd")
 
 const modloader_version := "v0.2.0"
-const expected_versions := ["v0.6.3", "v0.6.4"]
+const expected_versions := ["v0.6.3", "v0.6.4", "v0.6.5"]
 var game_version: String = "<game version not determined yet>"
 
 var exe_dir := OS.get_executable_path().get_base_dir()
