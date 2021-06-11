bc1f1a0fafab6b28b28b7aee9f2089712aa970ea
Valgo <valgohopscotch@gmail.com>
2021-06-11 12:40:01 -0600

    Update to content patch 7

--- luckyapi/modloader/patches/Reels.gd
+++ luckyapi/modloader/patches/Reels.gd
@@ -7,11 +7,10 @@ func update_icon_types():
     for r in reels:
         for i in r.icons:
             r.saved_icon_data[i.grid_position.y].persistent_data = i.persistent_data
+            i.has_effects = false
+
 
 func spin():
-    for r in reels:
-        for i in r.icons:
-            i.has_effects = false
     modloader.starting_symbols = []
     
     .spin()
