1a8d09245f6087424f9ae3cd635f0fe7af65a713
Valgo <valgohopscotch@gmail.com>
2021-07-12 13:22:36 -0600

    Fix crash with Banana Tree in Valgo's Content Pack

--- mods/valgos-content-pack/symbols/BananaTree.gd
+++ mods/valgos-content-pack/symbols/BananaTree.gd
@@ -15,4 +15,4 @@ func init(modloader: Reference, params):
     self.description = "Has a <color_E14A68><value_1>%<end> chance of <color_E14A68>adding<end> <all_or_banana_tree_adds>."
 
 func add_conditional_effects(symbol, adjacent):
-    symbol.add_effect(effect().if_value_random(0).add_symbol_type("banana_tree_adds").animate("bounce"))
+    symbol.add_effect(effect().if_value_random(0).add_symbol_group("banana_tree_adds").animate("bounce"))
