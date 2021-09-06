b43bc71e0d5c800e3bc45f4f7a9410e3fe5142b6
Fr0sty67 <jarrod.lever@gmail.com>
2021-09-06 21:21:35 +1000

    Patch 11 changes

--- luckyapi/modloader/patches/Reels.gd
+++ luckyapi/modloader/patches/Reels.gd
@@ -14,3 +14,18 @@ func spin():
     modloader.starting_symbols = []
     
     .spin()
+
+func swap_icon_positions(i1, i2):
+    var values_to_keep = ["persistent_data"]
+    var i1_data = {}
+    var i2_data = {}
+	
+    for v in values_to_keep:
+        i1_data[v] = i1[v]
+        i2_data[v] = i2[v]
+    
+    .swap_icon_positions(i1, i2)
+    
+    for v in values_to_keep:
+        i1[v] = i2_data[v]
+        i2[v] = i1_data[v]
